510e080f5b475b5dabe8732ab9b78964315920b4
izal <58200011+rzlahd@users.noreply.github.com>
2023-11-03 02:38:45 +0700

    Create ScrollingActivity.java

---
+++ Scrolling View/ScrollingActivity.java
@@ -0,0 +1,26 @@
+public class ScrollingActivity extends AppCompatActivity {
+
+    private ActivityScrollingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
