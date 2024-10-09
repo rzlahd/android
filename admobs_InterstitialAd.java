217e9e12a268195bcf7d4cebd53ee888a55aab3a
izal <58200011+rzlahd@users.noreply.github.com>
2024-10-09 08:05:38 +0700

    Create admobs_InterstitialAd.java

---
+++ admobs_InterstitialAd.java
@@ -0,0 +1,21 @@
+public void InterstitialAd_load(){
+      AdRequest adRequest = new AdRequest.Builder().build();
+
+      InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+        new InterstitialAdLoadCallback() {
+      @Override
+      public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+        // The mInterstitialAd reference will be null until
+        // an ad is loaded.
+        mInterstitialAd = interstitialAd;
+        Log.i(TAG, "onAdLoaded");
+      }
+
+      @Override
+      public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+        // Handle the error
+        Log.i(TAG, loadAdError.getMessage());
+        mInterstitialAd = null;
+      }
+    });
+}
